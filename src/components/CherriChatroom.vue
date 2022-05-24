35460b543412c6a00c2dbd1071285293e2ea1f0d
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-05-24 18:22:55 +0800

    20220524 Add DefaultView

---
+++ src/components/CherriChatroom.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-100">
+    <HeaderBar />
+    <component :is="'DefaultView'" />
+  </div>
+</template>
+
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
+import DefaultView from "@/components/DefaultView.vue"
+
+export default {
+  name: "CherriChatroom",
+  components: {
+    HeaderBar,
+    DefaultView
+  },
+  props: {
+    userId: {
+      type: String,
+    },
+  },
+};
+</script>
---
+++ src/components/DefaultView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-100 h-100">
+        <div class="text-center default-align">
+            <img width="180" height="150" src="../assets/images/img_default.png" alt="default image">
+            <p class="text-h5 text-theme mt-2">開始使用 Cherri Chat</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DefaultView"
+}
+</script>
