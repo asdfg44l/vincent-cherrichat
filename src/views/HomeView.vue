989e85e4e730774cb01d1c070cab3e61d1925961
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-05-24 17:24:15 +0800

    20220524 Adjust column to no-gutters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container class="d-flex vh-100">
-    <v-row :dense="true">
+  <v-container class="d-flex vh-100 py-0">
+    <v-row :dense="true" no-gutters>
       <!-- friends-list -->
-      <v-col cols="4" md="3" class="px-0 py-0">
+      <v-col cols="4" md="3" class="py-0">
         <friends-list class="h-100" />
       </v-col>
       <!-- chat-content -->
-      <v-col cols="8" md="9" class="px-0 py-0">
+      <v-col cols="8" md="9" class="py-0">
         <cherri-chatroom :userId="userId" />
       </v-col>
     </v-row>
